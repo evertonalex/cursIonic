12ee4c2b862c925a3d7ffc5f3fbc337b396050f6
EvertonAlex <evertonalexweb@gmail.com>
2018-07-14 21:59:18 -0300

    carregando produtos de uma dada categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ProdutoService } from './services/domain/produto.service';
 import { AuthInterceptorProvider } from './interceptors/auth-interceptor';
 import { ClienteService } from './services/domain/cliente.service';
 import { StorageService } from './services/storage.service';
@@ -37,7 +38,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     ErrorInterceptorProvider, 
     AuthService, 
     StorageService, 
-    ClienteService
+    ClienteService,
+    ProdutoService
   ]
 })
 export class AppModule {}
