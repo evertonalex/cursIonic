73cb1462aa30ba182469503ba26cdd4b9a567b87
EvertonAlex <evertonalexweb@gmail.com>
2018-07-22 10:28:21 -0300

    informando pedido registrado ao usuario - bug back não retornando location com o id pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -18,6 +18,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codPedido: string;
 
   constructor(
     public navCtrl: NavController,
@@ -51,17 +52,26 @@ export class OrderConfirmationPage {
   back(){
     this.navCtrl.setRoot('CartPage');
   }
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
 
   checkout(){
     this.pedidoService.insert(this.pedido).subscribe(response =>{
       this.cartService.createOrClearCart();
       console.log(response.headers.get('location'));
+      this.codPedido = this.extractId(response.headers.get('location'));
     }, 
     error=>{
       if(error.status == 403){
         this.navCtrl.setRoot('HomePage');
       }
     });
+  }
+
+  private extractId(location: string):string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 
 }
