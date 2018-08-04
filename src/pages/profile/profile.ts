6ef477d3918e828be1db9d9986a5bcc2dd2fd155
EvertonAlex <evertonalexweb@gmail.com>
2018-08-04 09:49:22 -0300

    uplaod com a camera - testar no device

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -4,6 +4,8 @@ import { StorageService } from './../../app/services/storage.service';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ClienteDTO } from '../../app/models/cliente.dto';
+import { CameraOptions } from '../../../node_modules/@ionic-native/camera';
+import { Camera } from '@ionic-native/camera';
 
 @IonicPage()
 @Component({
@@ -13,12 +15,15 @@ import { ClienteDTO } from '../../app/models/cliente.dto';
 export class ProfilePage {
 
   cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public storage: StorageService,
-    public ClienteService: ClienteService) {
+    public ClienteService: ClienteService,
+    public camera: Camera) {
   }
 
   ionViewDidLoad() {
@@ -44,6 +49,22 @@ export class ProfilePage {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
     },
     error=>{});
+  }
+
+  getCameraPicture(){
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality:100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData)=>{
+      let picture = 'data:image/png;base64' + imageData;
+      this.cameraOn = false;
+    }, (err)=>{
+
+    });
   }
 
 }
